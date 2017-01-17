f506971b8367a1bdc86c9298650b43843dcb4931
Rick Love <rick@toldpro.com>
2017-01-17 17:05:41 -0600

    Remove Unit Test Comments

--- src/work-hours.spec.ts
+++ src/work-hours.spec.ts
@@ -18,23 +18,9 @@ describe('processWorkHours with simple document', () => {
 `;
 
         result = processWorkHours(doc);
-        console.log('START beforeAll');
-        console.log('result', result);
-        console.log('result.totalHours', result.totalHours);
-        result.days.forEach(x => console.log('x.totalHours', x.totalHours));
-        result.days.forEach(x => console.log('x.dateText', x.dateText));
-        result.days.forEach(x => x.periods.forEach(p => console.log('p.totalHours', p.totalHours)));
-        result.days.forEach(x => x.periods.forEach(p => console.log('p.timeStartText', p.timeStartText)));
-        result.days.forEach(x => x.periods.forEach(p => console.log('p.timeStart', p.timeStart)));
-        result.days.forEach(x => x.periods.forEach(p => console.log('p.timeEndText', p.timeEndText)));
-        result.days.forEach(x => x.periods.forEach(p => console.log('p.timeEnd', p.timeEnd)));
     });
 
     it('should have correct total hours', () => {
-
-        console.log('START it');
-        console.log(result);
-
         expect(result.totalHours).toBe(2);
     });
 });
