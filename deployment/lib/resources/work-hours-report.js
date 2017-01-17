b8a59164ac802981ace7ca07a2b23a76c968bfb0
Rick Love <rick@toldpro.com>
2017-01-17 14:23:48 -0600

    Deploy

--- deployment/lib/resources/work-hours-report.js
+++ deployment/lib/resources/work-hours-report.js
@@ -42,16 +42,94 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ function(module, exports, __webpack_require__) {
 
 	"use strict";
+	var work_hours_1 = __webpack_require__(1);
 	function setup() {
 	    var host = document.createElement('div');
-	    host.innerHTML = 'TEST';
+	    host.innerHTML = 'LOADED';
 	    document.body.appendChild(host);
+	    var output = document.createElement('div');
+	    var input = document.createElement('textarea');
+	    input.rows = 10;
+	    input.style.width = '100%';
+	    input.addEventListener('change', function () {
+	        var text = input.value;
+	        var report = work_hours_1.processWorkHours(text);
+	        output.innerHTML = JSON.stringify(report);
+	    });
+	    host.innerHTML = '';
+	    host.appendChild(input);
+	    host.appendChild(output);
 	}
 	exports.setup = setup;
 	setup();
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	"use strict";
+	function processWorkHours(text) {
+	    var report = { days: [] };
+	    var lines = text.split('\n').map(function (x) { return x.trim(); }).filter(function (x) { return x.length > 0; });
+	    var day = null;
+	    var period = null;
+	    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+	        var l = lines_1[_i];
+	        if (l.match(/^# 20\d{2}/)) {
+	            // Date
+	            var dateText = l.replace('# ', '').trim();
+	            day = {
+	                dateText: dateText,
+	                date: new Date(Date.parse(dateText)),
+	                periods: [],
+	                totalHours: 0
+	            };
+	            report.days.push(day);
+	        }
+	        else if (l.match(/^## \d{1,2}:/)) {
+	            if (day == null) {
+	                console.warn('Time period has no day:', l);
+	                continue;
+	            }
+	            var parts = l.split('-');
+	            var timeStartText = parts[0].replace('## ', '').trim();
+	            var timeEndText = parts[1].trim();
+	            var timeStart = new Date(Date.parse(day.dateText + ' ' + timeStartText));
+	            var timeEnd = timeEndText.length > 0 ? new Date(Date.parse(day.dateText + ' ' + timeEndText)) : null;
+	            var total5Minutes = timeEnd != null ? (timeEnd.getTime() - timeStart.getTime()) / (5 * 60 * 1000) : 0;
+	            var totalHours = Math.round(total5Minutes) / 12;
+	            period = {
+	                timeStartText: timeStartText,
+	                timeStart: timeStart,
+	                timeEndText: timeEndText,
+	                timeEnd: timeEnd,
+	                totalHours: totalHours,
+	                tasks: []
+	            };
+	            day.periods.push(period);
+	        }
+	        else if (l.match('^#')) {
+	            // Ignore section
+	            day = null;
+	            period = null;
+	        }
+	        else {
+	            if (day == null) {
+	                console.warn('Task has no period:', l);
+	                continue;
+	            }
+	            period.tasks.push(l);
+	        }
+	    }
+	    report.days.forEach(function (d) { return d.totalHours = d.periods.reduce(function (out, p) { return out += p.totalHours; }, 0); });
+	    report.totalHours = report.days.reduce(function (out, d) { return out += d.totalHours; }, 0);
+	    return report;
+	}
+	exports.processWorkHours = processWorkHours;
 
 
 /***/ }
--- src-webpack/work-hours-report.ts
+++ src-webpack/work-hours-report.ts
@@ -2,8 +2,26 @@ import { processWorkHours } from './../src/work-hours';
 
 export function setup() {
     let host = document.createElement('div');
-    host.innerHTML = 'TEST';
+    host.innerHTML = 'LOADED';
     document.body.appendChild(host);
+
+    let output = document.createElement('div');
+    let input = document.createElement('textarea');
+
+    input.rows = 10;
+    input.style.width = '100%';
+
+    input.addEventListener('change', () => {
+        let text = input.value;
+
+        let report = processWorkHours(text);
+        output.innerHTML = JSON.stringify(report);
+    });
+
+
+    host.innerHTML = '';
+    host.appendChild(input);
+    host.appendChild(output);
 }
 
 setup();
