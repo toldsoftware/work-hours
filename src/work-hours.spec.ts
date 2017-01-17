917e82069d9fafa86889ade583898ca23bca7d7f
Rick Love <rick@toldpro.com>
2017-01-17 16:43:42 -0600

    TEST Travis

--- src/work-hours.spec.ts
+++ src/work-hours.spec.ts
@@ -18,9 +18,17 @@ describe('processWorkHours with simple document', () => {
 `;
 
         result = processWorkHours(doc);
+        console.log('beforeAll');
+        console.log(result);
+        console.log(result.totalHours);
+        console.log(result.days);
     });
 
     it('should have correct total hours', () => {
+
+        console.log('it');
+        console.log(result);
+
         expect(result.totalHours).toBe(2);
     });
 });
